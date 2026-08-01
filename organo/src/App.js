0c916cfe600c2c96e61f01748dc152b354e95b29
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-07-31 22:28:57 -0300

    Add TextField component

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,10 +1,11 @@
 import Banner from './components/Banner/Banner';
+import TextField from './components/TextField';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      
+      <TextField />
     </div>  
     );
 }
---
+++ organo/src/components/TextField/TextField.js
@@ -0,0 +1,8 @@
+export const TextField = () => {
+    return (
+        <div className="text-field">
+            <label>Nome</label>
+            <input placeholder="Digite o seu nome"></input>
+        </div>
+    )
+}
