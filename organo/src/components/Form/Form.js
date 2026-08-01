54ceb64868eb2eb626b6cfbf99521f87864e25cd
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-07-31 22:58:02 -0300

    Add Form component

---
+++ organo/src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import TextField from "../TextField"
+
+export const Form = () => {
+    return (
+        <section className="form">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <TextField label="Nome" placeholder="Digite o seu nome" />
+                <TextField label="Cargo" placeholder="Digite o seu cargo" />
+                <TextField label="Imagem" placeholder="Digite o endereço da imagem" /> 
+            </form>
+        </section>
+    )
+}
